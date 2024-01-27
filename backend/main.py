dc8d275b010aa209700e0b6c8076471c91bdd1fb
twleochao <leo.chao111@gmail.com>
2024-01-27 11:32:46 -0800

    flask user id

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
 import csv
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 HEADERS = ['Event Name', 'Event Image Src', 'Event Time', 'Event Location', 'Event Address']
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.model):
+    name = db.Column(db.String(30), primary_key=True)
+
+    def __repr__(self):
+        return f"User('{self.id}')"
+
 
 def removecommas(string):
     return string.replace(',', '')
@@ -24,3 +37,7 @@ def main():
         data = csv.reader(csvfile)
         get_data(data)
 
+if __name__ == '__main__':
+    main()
+    db.create_all()
+    app.run(debug=True)
